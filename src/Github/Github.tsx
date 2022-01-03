42ca1ac1e95170d9bb77e935d8d078287e63be80
Elya <elvira.kisling@gmail.com>
2022-01-03 22:12:59 +0300

    3 useEffects

---
+++ src/Github/Github.tsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import React, {useEffect, useState} from "react";
+import s from './Github.module.css';
+
+type SearchUserType = {
+    login: string
+    id: number
+}
+type SearchResult = {
+    items: SearchUserType[]
+}
+type UserType = {
+    login: string
+    id: number
+    avatar_url: string
+    followers: number
+}
+
+export const Github = () => {
+
+    const [selectedUser, setSelectorUser] = useState<SearchUserType | null>(null);
+    const [userDetails, setUserDetails] = useState<null | UserType>(null)
+    const [users, setUsers] = useState<SearchUserType[]>([]);
+    const [tempSearch, setTempSearch] = useState('it-kamasutra')
+    const [searchTerm, setSearchTerm] = useState('it-kamasutra')
+
+    useEffect(() => {
+        console.log("SYNC TAB TITLE")
+        if (selectedUser) {
+            document.title = selectedUser.login
+        }
+    }, [selectedUser])
+    useEffect(() => {
+        console.log("SYNC USERS")
+        axios.get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
+            .then(res => {
+                setUsers(res.data.items)
+            })
+    }, [searchTerm])
+    useEffect(() => {
+        console.log("SYNC USER DETAILS")
+        if (!!selectedUser) {
+            axios.get<UserType>(`https://api.github.com/search/users/${selectedUser.login}`)
+                .then(res => {
+                    setUserDetails(res.data)
+                })
+        }
+    }, [selectedUser])
+
+    return <div className={s.container}>
+        <div>
+            <div>
+                <input placeholder="search"
+                       value={tempSearch}
+                       onChange={(e) => {
+                           setTempSearch(e.currentTarget.value)
+                       }}
+                />
+                <button onClick={() => {
+                    setSearchTerm(tempSearch)
+                }}>find
+                </button>
+            </div>
+            <ul>
+                {users.map(u => <li key={u.id} className={selectedUser === u ? s.selected : ''}
+                                    onClick={() => {
+                                        setSelectorUser(u)
+                                    }}>
+                    {u.login}
+                </li>)}
+            </ul>
+        </div>
+        <div>
+            <h2>Username</h2>
+            {userDetails &&
+                <div>
+                    <img src={userDetails.avatar_url}/>
+                    <br/>
+                    {userDetails.login},
+                    followers: {userDetails.followers}
+                </div>}
+        </div>
+    </div>
+}
---
+++ src/Github/Github2.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Github2 = () => {
+    return <div>
+
+    </div>
+}
